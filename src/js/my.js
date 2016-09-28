79e5360f52120bd97a3396438902cfb7e479d374
Rutul Patel <rutul-patel@live.com>
2016-09-27 23:56:36 -0700

    fix smooth scroll issue with jquery

--- src/js/my.js
+++ src/js/my.js
@@ -18,19 +18,24 @@ function collapseNavbar() {
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
+
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
+    $('app-root').on('click', '.page-scroll', function(event) {
+        var anchor = $(this);
+
+        console.log($(anchor).offset());
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $(anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
+        
         event.preventDefault();
     });
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('app-root').on('click', '.navbar-collapse ul li a', function() {
   if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
     $('.navbar-toggle:visible').click();
   }
